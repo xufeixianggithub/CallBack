beaba3a56b5df0f3193f82b4d84d40afdb25a697
zude <zude@seeapp.com>
2020-01-07 18:59:00 +0800

    添加测试

--- src/main/java/com/interrupt/Funtion/consume/OperatorTest.java
+++ src/main/java/com/interrupt/Funtion/consume/OperatorTest.java
@@ -1,7 +1,22 @@
 package com.interrupt.Funtion.consume;
 
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
 public class OperatorTest {
     public static void main(String[] args) {
+        test2();
+    }
 
+    public static void test1(){
+        UnaryOperator<Integer> unaryOperator=(a)->a*a;
+        Integer b=unaryOperator.apply(2);
+        System.out.println(b);
+    }
+
+    public static void test2(){
+        BinaryOperator<Integer> binaryOperator=(a, b)->a*b;
+        Integer b=binaryOperator.apply(2,3);
+        System.out.println(b);
     }
 }
